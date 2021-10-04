ac9f86c46606d428506112350fa6ec4a20fd9daa
Carlos Zubieta <czubieta@etsy.com>
2021-10-04 13:52:52 -0700

    Remove comment out code

--- Formula/sewnutils.rb
+++ Formula/sewnutils.rb
@@ -2,7 +2,6 @@ class Sewnutils < Formula
   desc "Set of small tailor-made tools."
   homepage "https://github.com/zubieta/sewnutils"
   url "https://github.com/zubieta/sewnutils/archive/v0.3.tar.gz"
-  # sha256 "0accd5aa87e9bb36401cef3b38ae4ba76288d3b869913ae74d509b666f3f4b4b"
   sha256 "27462cde7338632154f418c2eae4cddd2bf6ca96dd7226088c64a5212d367ad7"
   head "https://github.com/zubieta/sewnutils.git"
 
